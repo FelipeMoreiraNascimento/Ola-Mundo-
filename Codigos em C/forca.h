c28d1dd78e1d20674475097f580f208733d97c6a
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-05 11:58:03 -0300

    Create forca.h

---
+++ Codigos em C/forca.h
@@ -0,0 +1,9 @@
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int enforcou();
+int acertou();
+void adicionapalavra();
+#define TAMANHO_PALAVRA 20
