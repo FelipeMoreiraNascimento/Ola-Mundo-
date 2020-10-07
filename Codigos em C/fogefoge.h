e8cc1ba43406eb975089e8c26834d0addb19938e
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-07 11:33:47 -0300

    Update fogefoge.h

--- Codigos em C/fogefoge.h
+++ Codigos em C/fogefoge.h
@@ -1,3 +1,15 @@
 void liberamapa();
 void lemapa();
 void alocamapa();
+int acabou();
+void imprimemapa();
+void move(char direcao);
+
+struct mapa
+{
+    char** matriz;
+    int linhas;
+    int colunas;
+};
+
+typedef struct mapa MAPA;
