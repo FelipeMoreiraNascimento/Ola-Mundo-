fa1524babe9319d1efaea06dd8bc70517be66569
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-09 13:22:11 -0300

    Create fogefoge.c

---
+++ Codigos em C/jogo fogefoge/fogefoge.c
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "fogefoge.h"
+#include "mapa.h"
+
+MAPA m;
+POSICAO heroi;
+int acabou()
+{
+    return 0;
+}
+
+void move(char direcao)
+{
+    if (direcao!='a' && direcao != 'w' && direcao !='s' && direcao != 'd') return;
+    
+    int proximox = heroi.x;
+    int proximoy = heroi.y;
+
+    switch (direcao)
+    {
+    case 'a':
+        proximoy--;
+        break;
+    case 'w':
+        proximox--;
+        break;
+    case 's':
+        proximox++;
+        break;
+    case 'd':
+        proximoy++;
+        break;
+    } 
+
+    if (proximox >= m.linhas) 
+        return;
+    if (proximoy >=m.colunas) 
+        return;
+    if (m.matriz[proximox][proximoy] !='.') 
+        return;
+    
+    m.matriz[proximox][proximoy] = '@';
+    m.matriz[heroi.x][heroi.y] = '.';
+    heroi.x = proximox;
+    heroi.y = proximoy;        
+}
+
+int main()
+{
+    lemapa(&m);
+    encontramapa(&m, &heroi, '@');
+
+    do
+    {
+        imprimemapa(&m);
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+    
+    }while(!acabou());
+    
+    liberamapa(&m);
+}
