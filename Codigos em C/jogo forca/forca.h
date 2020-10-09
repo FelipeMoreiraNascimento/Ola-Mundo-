14d12dedf45b39458e7aa02f39e53e618a99173c
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-09 13:21:46 -0300

    Create forca.h

---
+++ Codigos em C/jogo forca/forca.h
@@ -0,0 +1,10 @@
+void abertura();
+void chuta();
+int jachutou(char letra);
+void desenhaforca();
+void escolhepalavra();
+int enforcou();
+int acertou();
+void adicionapalavra();
+int chuteserrados();
+#define TAMANHO_PALAVRA 20
