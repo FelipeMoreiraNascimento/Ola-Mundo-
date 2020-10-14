4009c612fba00764934d743fdd1907eb661024c6
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-13 21:32:00 -0300

    Update mapa.h

--- Codigos em C/jogo fogefoge/mapa.h
+++ Codigos em C/jogo fogefoge/mapa.h
@@ -1,3 +1,9 @@
+#define HEROI  '@'
+#define FANTASMA 'F'
+#define VAZIO '.'
+#define PAREDE_VERTICAL '|'
+#define PAREDE_HORIZONTAL '-'
+
 struct mapa
 {
     char** matriz;
@@ -19,3 +25,8 @@ void lemapa(MAPA* m);
 void alocamapa(MAPA* m);
 void imprimemapa(MAPA* m);
 void encontramapa(MAPA* m, POSICAO* p, char c);
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA* m, int x, int y);
+void andanomapa(MAPA* m, int xorigem, int yorigem, int xdestino, int ydestino);
+void copiamapa(MAPA* destino, MAPA* origem);
+
