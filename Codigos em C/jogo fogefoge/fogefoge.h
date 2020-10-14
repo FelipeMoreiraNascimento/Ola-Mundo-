aeec25a26d3bbcba795cc0679e1e7582ae734d37
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-13 21:32:08 -0300

    Update fogefoge.h

--- Codigos em C/jogo fogefoge/fogefoge.h
+++ Codigos em C/jogo fogefoge/fogefoge.h
@@ -1,3 +1,11 @@
+#define CIMA 'w'
+#define BAIXO 's'
+#define ESQUERDA 'a'
+#define DIREITA 'd'
+
+
 int acabou();
 void move(char direcao);
-
+int ehdirecao(char direcao);
+void fantasmas();
+int praondeofantasmavai(int xatual, int yatual, int* xdestino, int*ydestino);
