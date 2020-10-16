0a3d25e952b29f486e129b558f99921340dd3c29
Felipe Nascimento <68857942+FelipeMoreiraNascimento@users.noreply.github.com>
2020-10-16 10:22:17 -0300

    Update fogefoge.c

--- Codigos em C/jogo fogefoge/fogefoge.c
+++ Codigos em C/jogo fogefoge/fogefoge.c
@@ -6,6 +6,7 @@
 
 MAPA m;
 POSICAO heroi;
+int tempilula = 0;
 
 int praondeofantasmavai(int xatual, int yatual, int* xdestino, int*ydestino)
 {
@@ -104,11 +105,46 @@ void move(char direcao)
         return;
     }
 
+    if (ehpersonagem(&m, PILULA, proximox, proximoy))
+    {
+        tempilula = 1;
+    }
+    
+
     andanomapa(&m, heroi.x, heroi.y, proximox, proximoy);
     heroi.x = proximox;
     heroi.y = proximoy;
 
 }
+
+void explodepilula()
+{   
+    if(!tempilula) return;
+    explodepilula2(heroi.x, heroi.y, 0, 1, 3);
+    explodepilula2(heroi.x, heroi.y, 0, -1, 3);
+    explodepilula2(heroi.x, heroi.y, 1, 0, 3);
+    explodepilula2(heroi.x, heroi.y, -1, 0, 3);
+
+    tempilula = 0;
+}
+
+
+
+void explodepilula2(int x, int y,int somax, int somay, int qtd)
+{
+    if(qtd == 0) return;
+
+    int novox = x + somax;
+    int novoy = x + somay;
+
+
+    if(!ehvalida(&m, novox, novoy)) return;
+    if(ehparede(&m, novox, novoy)) return;
+
+    m.matriz[x][y+1] = VAZIO;
+    explodepilula2(novox, novoy, somax, somay, qtd - 1);
+}
+
 
 int main()
 {
@@ -117,10 +153,17 @@ int main()
     printf("fogefoge\n\n\n");
     do
     {
+        printf("Tem pilula: %s\n", (tempilula ? "SIM" : "NAO"));
         imprimemapa(&m);
         char comando;
         scanf(" %c", &comando);
         move(comando);
+        if (comando == BOMBA)
+        {
+            explodepilula();
+        }
+
+        
         fantasmas();
     
     }while(!acabou());
